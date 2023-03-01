c17d1d8dc3068eefa3d5e90cdf663b031dbab5e9
benjpujol <40798113+benjpujol@users.noreply.github.com>
2023-03-01 18:14:30 +0100

    Update settings.py

--- heatpump-backend/settings.py
+++ heatpump-backend/settings.py
@@ -34,7 +34,8 @@ CORS_ORIGIN_WHITELIST = [
     "http://localhost:3001",
     "http://127.0.0.1:3001", 
     "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000", 
+    "https://heatpump-frontend.vercel.app/"
     # your React App domain
 ]
 
