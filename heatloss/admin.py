abd55c7d0b82c7567ffa5edfa913de93d8c42b90
benjpujol <40798113+benjpujol@users.noreply.github.com>
2023-03-01 19:50:38 +0100

    Update admin.py

--- heatloss/admin.py
+++ heatloss/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Wall, Roof, Floor, Window
+
+admin.site.register(Wall)
+admin.site.register(Roof)
+admin.site.register(Floor)
+admin.site.register(Window)
