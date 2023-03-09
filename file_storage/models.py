5ee06ef9ccbc1ec4d9c302eb79d96621e9885a62
benjpujol <40798113+benjpujol@users.noreply.github.com>
2023-03-09 20:48:14 +0100

    filestorage model

--- file_storage/models.py
+++ file_storage/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from users.models import Settings
+
 
 # Create your models here.
 class File(models.Model):
@@ -7,3 +9,27 @@ class File(models.Model):
 
     def __str__(self):
         return self.file.name
+    
+
+def logo_upload_path(instance, filename):
+    pass
+
+class Logo(models.Model):
+    image = models.ImageField(upload_to='logos')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.image.name
+    
+    def save(self, *args, **kwargs):
+        settings = kwargs.pop('settings', 0)
+        if settings :
+            # Fetch the related Settings instance from the settings parameter
+    
+            # Get the user from the related Settings instance
+            user_id = settings.user.id
+            # Construct the path using the user_id and current timestamp
+            upload_path = f'{user_id}/{self.image.name}'
+            print(upload_path)
+            self.image.name = upload_path
+        super().save(*args, **kwargs)
--- file_storage/views.py
+++ file_storage/views.py
@@ -2,8 +2,8 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.core.files.storage import default_storage
 from google.cloud import storage
-
-from .models import File
+from users.models import Settings
+from .models import File, Logo
 
 @csrf_exempt
 def upload_file(request):
@@ -37,6 +37,61 @@ def upload_file(request):
             response_data = {'success': True, 'file_url': file_url}
         else:
             response_data = {'success': False, 'error': 'No file was received.'}
+    else:
+        response_data = {'success': False, 'error': 'Only POST requests are allowed.'}
+
+    return JsonResponse(response_data)
+
+@csrf_exempt
+def upload_logo(request):
+    if request.method == 'POST':
+        print(request.FILES)
+        user_id = request.POST.get('user_id')  # Assuming the user is authenticated
+        logo = request.FILES.get('logo')
+        print(request.FILES.get('logo'))
+
+        if logo :
+            settings_instance = Settings.objects.get(user_id=user_id)
+            # Create a new File instance and save it to the database
+            logo_instance = Logo(image=logo)
+            logo_instance.save(settings = settings_instance)
+
+            # Get the default Google Cloud Storage bucket
+            client = storage.Client()
+            bucket = client.get_bucket('vesta-users-files')
+
+            # Define the path for the uploaded file in Google Cloud Storage
+            
+            filename = f"{logo_instance.image.name}"
+
+            print("filename",filename)
+
+            logo.seek(0)
+            
+            # Create a Blob object and upload the file to Google Cloud Storage
+            blob = bucket.blob(filename)
+            blob.upload_from_string(logo.read(), content_type=logo.content_type)
+
+            # Get the public URL for the uploaded file
+            file_url = blob.public_url
+
+            # Update the File instance with the public URL
+            logo_instance.file_url = file_url
+            logo_instance.save()
+
+            # add the file to the user settings model
+            settings_instance.logo = logo_instance
+            settings_instance.save()
+
+
+            response_data = {'success': True, 'file_url': file_url}
+
+          
+            
+
+            response_data = {'success': True, 'logo_url':file_url}
+        else:
+            response_data = {'success': False, 'error': 'No logo was received.'}
     else:
         response_data = {'success': False, 'error': 'Only POST requests are allowed.'}
 
